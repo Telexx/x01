8d1d1807e56cf56b5d877b5ac6398a22b9477e36
Alec Henderson <alec.henderson@gamma.co.uk>
2021-02-14 18:03:57 +0000

    More cool stuff

--- x01/GameSettingsView.swift
+++ x01/GameSettingsView.swift
@@ -11,7 +11,8 @@ struct GameSettingsView: View {
     @State private var selectedGameType = "501"
     @State private var selectedOpponentLevel = 6
     @State private var playerThrowsFirst = true
-    @State private var numberOfLegs = 1.0
+    @State private var numberOfLegs = 7.0
+    @State private var adaptiveAI = false
     let gameTypes = ["501", "301", "701", "1001"]
     let opponentLevels = [1,2,3,4,5,6,7,8,9,10]
     var body: some View {
@@ -24,10 +25,21 @@ struct GameSettingsView: View {
                         }
                     }
                     Toggle("Player throws first?", isOn: $playerThrowsFirst)
-                    Slider(value: $numberOfLegs, in: 0...100, step: 1)
+                    Slider(value: $numberOfLegs, in: 1...30, step: 2)
+                    Text("Best of \(Int(numberOfLegs)) leg(s)")
                 }
                 Section(header: Text("OPPONENT LEVEL")) {
-
+                    Toggle("Adaptive AI? 🤖", isOn: $adaptiveAI)
+                    if !adaptiveAI{
+                        Picker("Select AI level 🤖", selection: $selectedOpponentLevel){
+                            ForEach(opponentLevels, id: \.self) {
+                                Text(String($0))
+                            }
+                        }
+                    }
+                    else{
+                        Text("The AI will give you a good game, playing close to your level.").font(.callout).foregroundColor(.gray)
+                    }
                 }
                 
                 Section {
